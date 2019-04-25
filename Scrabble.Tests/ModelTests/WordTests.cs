57e2632e27df84f9ccc23def803478a370127c1c
Darrion Gering and Liz Kelley <liz@>
2019-04-25 13:35:30 -0700

    add compare method, add array compare test

--- Scrabble.Tests/ModelTests/WordTests.cs
+++ Scrabble.Tests/ModelTests/WordTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrabbleStuff;
 using System;
+using System.Collections.Generic;
 
 namespace Scrabble.Tests
 {
@@ -14,20 +15,29 @@ namespace Scrabble.Tests
       Assert.AreEqual(typeof(Words), testWords.GetType());
     }
 
-    // [TestMethod]
-    // public void MakeArray_StringReturnsLetterArray_True()
-    // {
-    //   Words testWords = new Words("word");
-    //   Assert.AreEqual('w', testWords.MakeArray());
-    // }
+    [TestMethod]
+    public void MakeArray_StringReturnsLetterArray_True()
+    {
+      char[] testArray = {'w', 'o', 'r', 'd'};
+      Words testWords = new Words("word");
+      CollectionAssert.AreEqual(testArray, testWords.MakeArray());
+    }
 
     [TestMethod]
     public void WordValue_FindLetterValue_Int()
     {
       Words testWords = new Words("word");
-      int value = testWords.GetValues("w");
+      int value = testWords.GetValues('w');
       Assert.AreEqual(4, value);
     }
 
+    [TestMethod]
+    public void Compare_CompareWordToDictionary_Int()
+    {
+      Words testWords = new Words("word");
+      char[] charArray = testWords.MakeArray();
+      int totalScore = testWords.Compare(charArray);
+      Assert.AreEqual(8, totalScore);
+    }
   }
 }
--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -5,10 +5,10 @@ namespace ScrabbleStuff
   public class Words
   {
     private string _word;
-    private static Dictionary<string, int> _letterPoints = new Dictionary<string, int> ()
+    private static Dictionary<char, int> _letterPoints = new Dictionary<char, int> ()
     {
-      {"a", 1}, {"e", 1}, {"i", 1}, {"o", 1}, {"u", 1}, {"l", 1}, {
-      "n", 1}, {"r", 1}, {"s", 1}, {"t", 1}, {"d", 2}, {"g", 2}, {"b", 3}, {"c", 3}, {"m", 3}, {"p", 3}, {"f", 4}, {"h", 4}, {"v", 4}, {"w", 4}, {"y", 4}, {"k", 5}, {"j", 8}, {"x", 8}, {"q", 10}, {"z", 10}
+      {'a', 1}, {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1}, {'l', 1}, {
+      'n', 1}, {'r', 1}, {'s', 1}, {'t', 1}, {'d', 2}, {'g', 2}, {'b', 3}, {'c', 3}, {'m', 3}, {'p', 3}, {'f', 4}, {'h', 4}, {'v', 4}, {'w', 4}, {'y', 4}, {'k', 5}, {'j', 8}, {'x', 8}, {'q', 10}, {'z', 10}
     };
 
     public Words(string word)
@@ -21,7 +21,7 @@ namespace ScrabbleStuff
       return _word;
     }
 
-    public int GetValues(string letter)
+    public int GetValues(char letter)
     {
       return _letterPoints[letter];
     }
@@ -36,6 +36,17 @@ namespace ScrabbleStuff
       _word = _word.ToLower();
       char[] charArr = _word.ToCharArray();
       return charArr;
+    }
+
+    public int Compare(char[] charArray)
+    {
+      int totalScore = 0;
+      for(int i = 0; i < charArray.Length; i++)
+      {
+        int tempScore = GetValues(charArray[i]);
+        totalScore += tempScore;
+      }
+      return totalScore;
     }
   }
 }
